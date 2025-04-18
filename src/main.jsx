9fe02af054fadd922ca095630167733cba5e1ab1
skystarfromm78 <skystarfromm78@gmail.com>
2025-04-18 02:40:19 +0000

    回退

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./App.css";
 import App from "./App.jsx";
-import "./index.css"; // 记得引入样式
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
